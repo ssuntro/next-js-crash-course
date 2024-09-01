fb35e809a07962b32caa5ee8b68199db78d80c31
Siroratt Suntronsuk <soulyu_shine@hotmail.com>
2024-09-01 23:01:55 +0700

    add NewPost component

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
-import Post from "./components/Post";
+
 import PostsList from "./components/PostsList";
+import NewPost from "./components/NewPost";
 function App() {
   return (<main>
     <h1>Hello World!</h1>
-   <PostsList/>
+    <NewPost/>
+    <PostsList/>
      
   </main>);
 }
--- src/components/PostsList.jsx
+++ src/components/PostsList.jsx
@@ -3,11 +3,14 @@ import classes from "./PostsList.module.css";
 
 function PostsList() {
     return (
+        <>
+        
         <ul className={classes.posts}> 
             <Post author="AnnJa" body="body1"/>
             <Post author="Awesome" body="body2"/>
             <Post author="Aoi"/>
         </ul>
+        </>
     )
 }
 
