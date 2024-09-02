14c651ac8d3a08f1e8f18036befb7f0b68f23873
Siroratt Suntronsuk <soulyu_shine@hotmail.com>
2024-09-02 17:02:23 +0700

    add show/hid Modal logic in App

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,24 @@
-
-import PostsList from "./components/PostsList";
+import MainHeader from './components/MainHeader'
+import PostsList from './components/PostsList'
+import { useState } from 'react'
 
 function App() {
-  return (<main>
-    <h1>Hello World!</h1>
-    
-    <PostsList/>
-     
-  </main>);
+  const [modalIsVisible, setModalIsVisible] = useState(false)
+  function showModalHandler() {
+    setModalIsVisible(true)
+  }
+  function hideModalHandler() {
+    setModalIsVisible(false)
+  }
+  return (
+    <>
+    <MainHeader onCreatePost={showModalHandler}/>
+    <main>
+      <h1>Hello World!</h1>
+      <PostsList onStopPosting={hideModalHandler} isPosting={modalIsVisible}/>
+    </main>
+    </>
+  )
 }
 
-export default App;
+export default App
--- src/components/PostsList.jsx
+++ src/components/PostsList.jsx
@@ -4,13 +4,11 @@ import NewPost from './NewPost'
 import { useState } from 'react'
 import Modal from './Modal'
 
-function PostsList() {
-  const [modalIsVisible, setModalIsVisible] = useState(true)
+function PostsList({isPosting, onStopPosting}) {
+  
   const [enteredBody, setEnteredBody] = useState('')
   const [enteredAuthor, setEnteredAuthor] = useState('')
-  function hideModalHandler() {
-    setModalIsVisible(false)
-  }
+  
 
   function bodyChangeHandler(event) {
     setEnteredBody(event.target.value)
@@ -21,8 +19,8 @@ function PostsList() {
   }
   return (
     <>
-      {modalIsVisible ? (
-        <Modal onClose={hideModalHandler}>
+      {isPosting ? (
+        <Modal onClose={onStopPosting}>
           <NewPost
             onBodyChange={bodyChangeHandler}
             onAuthorChange={authorChangeHandler}
