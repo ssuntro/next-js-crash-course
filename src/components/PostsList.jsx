aa8051538515c8b758a5983f9eda3665536b5bab
Siroratt Suntronsuk <soulyu_shine@hotmail.com>
2024-09-02 16:25:38 +0700

    PostsList, hide Modal when bg is clicked

--- src/components/PostsList.jsx
+++ src/components/PostsList.jsx
@@ -1,31 +1,43 @@
-import Post from "./Post";
-import classes from "./PostsList.module.css";
-import NewPost from "./NewPost";
-import { useState } from 'react';
-import Modal from "./Modal";
+import Post from './Post'
+import classes from './PostsList.module.css'
+import NewPost from './NewPost'
+import { useState } from 'react'
+import Modal from './Modal'
 
 function PostsList() {
-    const [enteredBody, setEnteredBody] = useState('');
-    const [enteredAuthor, setEnteredAuthor] = useState('');
-    function bodyChangeHandler(event) {
-        setEnteredBody(event.target.value);
-    }
-    
-    function authorChangeHandler(event) {
-        setEnteredAuthor(event.target.value);
-    }
-    return (
-        <>
-        <Modal>
-            <NewPost onBodyChange={bodyChangeHandler} onAuthorChange={authorChangeHandler}/>
+  const [modalIsVisible, setModalIsVisible] = useState(true)
+  const [enteredBody, setEnteredBody] = useState('')
+  const [enteredAuthor, setEnteredAuthor] = useState('')
+  function hideModalHandler() {
+    setModalIsVisible(false)
+  }
+
+  function bodyChangeHandler(event) {
+    setEnteredBody(event.target.value)
+  }
+
+  function authorChangeHandler(event) {
+    setEnteredAuthor(event.target.value)
+  }
+  return (
+    <>
+      {modalIsVisible ? (
+        <Modal onClose={hideModalHandler}>
+          <NewPost
+            onBodyChange={bodyChangeHandler}
+            onAuthorChange={authorChangeHandler}
+          />
         </Modal>
-        <ul className={classes.posts}> 
-            <Post author={enteredAuthor} body={enteredBody}/>
-            <Post author="Awesome" body="body2"/>
-            <Post author="Aoi"/>
-        </ul>
-        </>
-    )
+      ) : null} 
+      {/* null or undefined or false are display nothing */}
+
+      <ul className={classes.posts}>
+        <Post author={enteredAuthor} body={enteredBody} />
+        <Post author='Awesome' body='body2' />
+        <Post author='Aoi' />
+      </ul>
+    </>
+  )
 }
 
-export default PostsList;
+export default PostsList
