135314284064a50bfaedda1ff729078ad23d82de
Siroratt Suntronsuk <soulyu_shine@hotmail.com>
2024-09-03 14:19:57 +0700

    create RootLayout and move MainHeader to share across the app

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,24 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import NewPost from './components/NewPost'
+import RootLayout from './routes/RootLayout'
 
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <RootLayout />,
+    children: [
+      { path: '/', element: <App /> },
+      { path: '/create-post', element: <NewPost /> },
+      
+    ],
+  },
+]);
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    {/* <App /> */}
+    <RouterProvider router={router} />
   </React.StrictMode>
 )
--- src/routes/RootLayout.jsx
+++ src/routes/RootLayout.jsx
@@ -1,11 +1,14 @@
-import { Outlet } from "react-router-dom";
-import MainHeader from "../components/MainHeader";
+import { Outlet } from 'react-router-dom';
+import MainHeader from '../components/MainHeader';
 
 function RootLayout() {
+    return (
     <>
         <MainHeader />
         <Outlet />
-    </>
+    </>)
 }
 
 export default RootLayout;
+
+
