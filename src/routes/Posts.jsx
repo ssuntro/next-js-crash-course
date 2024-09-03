704020b5b7ee50e8686a125fc38846cd8ca5672c
Siroratt Suntronsuk <soulyu_shine@hotmail.com>
2024-09-03 15:56:31 +0700

    rename file, App to Posts

---
+++ src/routes/Posts.jsx
@@ -0,0 +1,25 @@
+import MainHeader from '../components/MainHeader'
+import PostsList from '../components/PostsList'
+import { useState } from 'react'
+
+function Posts() {
+  // const [modalIsVisible, setModalIsVisible] = useState(false)
+  // function showModalHandler() {
+  //   setModalIsVisible(true)
+  // }
+  // function hideModalHandler() {
+  //   setModalIsVisible(false)
+  // }
+  return (
+    <>
+    {/* <MainHeader onCreatePost={showModalHandler}/> */}
+    <main>
+      <PostsList 
+      // onStopPosting={hideModalHandler} isPosting={modalIsVisible}
+      />
+    </main>
+    </>
+  )
+}
+
+export default Posts
