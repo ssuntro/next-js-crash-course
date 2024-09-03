ee94c268c93ea152574e320b37f2dcac49d72cdb
Siroratt Suntronsuk <soulyu_shine@hotmail.com>
2024-09-03 16:12:18 +0700

    rewrite, move NewPost modal to another routing

--- src/routes/Posts.jsx
+++ src/routes/Posts.jsx
@@ -14,6 +14,7 @@ function Posts() {
   return (
     <>
     {/* <MainHeader onCreatePost={showModalHandler}/> */}
+    <Outlet />
     <main>
       <PostsList 
       // onStopPosting={hideModalHandler} isPosting={modalIsVisible}
